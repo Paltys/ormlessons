584c1629a7be41df014f8e7e4c3486c544bb1d1a
Paltys <Paltys01@gmail.com>
2019-03-23 02:15:35 +0200

    add error page directive

--- Ls1-1/src/servlets/loadServlet.java
+++ Ls1-1/src/servlets/loadServlet.java
@@ -1,19 +1,34 @@
 package servlets;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.servlet.http.Part;
+import java.io.*;
+import java.nio.file.Paths;
+import javax.servlet.annotation.WebServlet;
 
+/*@WebServlet("/loadServlet")
+@MultipartConfig*/
 
 public class loadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Part filePart = request.getPart("file");
+        String fileName = Paths.get(filePart.getSubmittedFileName()).toString();
+        System.out.println(fileName);
+        InputStream fileContent = filePart.getInputStream();
+        byte[] data = new byte[fileContent.available()];
+        DataInputStream dis = new DataInputStream(fileContent);
+        dis.readFully(data);
+        String relativePath = getServletContext().getRealPath("")+"files\\"+fileName;
+        System.out.println(relativePath);
+        FileOutputStream uploaded = new FileOutputStream(relativePath);
+        uploaded.write(data);
+        uploaded.close();
+        fileContent.close();
+        response.getWriter().print("OK "+fileName+" uploaded");
 
+        }
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-}
